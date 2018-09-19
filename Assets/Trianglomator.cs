f613b1fde50683a0bc707543dd3b3b7a42904b5f
Aras Pranckevicius <aras@unity3d.com>
2018-09-19 18:12:27 +0300

    Make core more robust wrt group sizes etc (query them), tweak RNG state persistence in CS, etc

--- Assets/Trianglomator.cs
+++ Assets/Trianglomator.cs
@@ -2,26 +2,29 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 
+[StructLayout(LayoutKind.Sequential)]
 public struct Triangle
 {
 	public Vector2 posA, posB, posC;
 	public Color col;
 }
 
+[StructLayout(LayoutKind.Sequential)]
 public struct Params
 {
 	public int triCount;
-	public int frame;
 	public int width;
 	public int height;
 }
 
+[StructLayout(LayoutKind.Sequential)]
 public struct Score
 {
 	public uint score;
 	public uint bestScore;
 	public uint iterations;
 	public uint improvements;
+	public uint rng;
 }
 
 public class Trianglomator : MonoBehaviour
@@ -41,9 +44,11 @@ public class Trianglomator : MonoBehaviour
 	ComputeBuffer m_Params;
 	ComputeBuffer m_Score;
 	Material m_Material;
-	int m_Counter;
-	int m_GotFitness = 90;
+	int m_GotFitness = 92;
 	string m_Log = "";
+
+	int m_KernelCopy, m_KernelMutate, m_KernelScore;
+	uint m_GroupsizeCopy, m_GroupsizeScoreX, m_GroupsizeScoreY;
 
 	void OnDestroy()
 	{
@@ -70,6 +75,13 @@ public class Trianglomator : MonoBehaviour
 
 		if (!m_RT)
 		{
+			m_KernelCopy = m_CS.FindKernel("CSCopy");
+			m_KernelMutate = m_CS.FindKernel("CSMutate");
+			m_KernelScore = m_CS.FindKernel("CSCalcScore");
+			uint dummyY, dummyZ;
+			m_CS.GetKernelThreadGroupSizes(m_KernelCopy, out m_GroupsizeCopy, out dummyY, out dummyZ);
+			m_CS.GetKernelThreadGroupSizes(m_KernelScore, out m_GroupsizeScoreX, out m_GroupsizeScoreY, out dummyZ);
+
 			var desc = new RenderTextureDescriptor
 			{
 				width = sourceTex.width,
@@ -85,15 +97,16 @@ public class Trianglomator : MonoBehaviour
 			m_RTBest = new RenderTexture(desc);
 			m_RTBest.Create();
 			m_BestImage.texture = m_RTBest;
-			m_CS.SetTexture(2, "_SourceTex", sourceTex);
-			m_CS.SetTexture(2, "_DestTex", m_RT);
+			m_CS.SetTexture(m_KernelScore, "_SourceTex", sourceTex);
+			m_CS.SetTexture(m_KernelScore, "_DestTex", m_RT);
+			m_Log = $"{sourceTex.name} tris: {m_Triangles}\n";
 		}
 
 		if (m_DNA == null)
 		{
 			m_DNA = new ComputeBuffer(m_Triangles * 2, Marshal.SizeOf(typeof(Triangle)));
 			var initData = new Triangle[m_Triangles * 2];
-			Random.seed = 1;
+			Random.InitState(1);
 			for (var i = 0; i < m_Triangles; ++i)
 			{
 				initData[i].posA = new Vector2(Random.value, Random.value);
@@ -103,30 +116,38 @@ public class Trianglomator : MonoBehaviour
 				initData[i + m_Triangles] = initData[i];
 			}
 			m_DNA.SetData(initData);
-			m_CS.SetBuffer(0, "_DNA", m_DNA);
-			m_CS.SetBuffer(1, "_DNA", m_DNA);
+			m_CS.SetBuffer(m_KernelCopy, "_DNA", m_DNA);
+			m_CS.SetBuffer(m_KernelMutate, "_DNA", m_DNA);
 		}
 
 		if (m_Params == null)
 		{
 			m_Params = new ComputeBuffer(1, Marshal.SizeOf(typeof(Params)));
-			m_CS.SetBuffer(0, "_Params", m_Params);
-			m_CS.SetBuffer(1, "_Params", m_Params);
-			m_CS.SetBuffer(2, "_Params", m_Params);
+			var para = new Params[1];
+			para[0].triCount = m_Triangles;
+			para[0].width = sourceTex.width;
+			para[0].height = sourceTex.height;
+			m_Params.SetData(para);
+
+			m_CS.SetBuffer(m_KernelCopy, "_Params", m_Params);
+			m_CS.SetBuffer(m_KernelMutate, "_Params", m_Params);
+			m_CS.SetBuffer(m_KernelScore, "_Params", m_Params);
 		}
 
 		var sc = new Score[1];
 		if (m_Score == null)
 		{
 			m_Score = new ComputeBuffer(1, Marshal.SizeOf(typeof(Score)));
-			m_CS.SetBuffer(0, "_Score", m_Score);
-			m_CS.SetBuffer(1, "_Score", m_Score);
-			m_CS.SetBuffer(2, "_Score", m_Score);
 			sc[0].score = 0xFFFFFFFF;
 			sc[0].bestScore = 0xFFFFFFFF;
 			sc[0].iterations = 0;
 			sc[0].improvements = 0;
+			sc[0].rng = 1;
 			m_Score.SetData(sc);
+
+			m_CS.SetBuffer(m_KernelCopy, "_Score", m_Score);
+			m_CS.SetBuffer(m_KernelMutate, "_Score", m_Score);
+			m_CS.SetBuffer(m_KernelScore, "_Score", m_Score);
 		}
 
 		if (!m_Material)
@@ -135,17 +156,10 @@ public class Trianglomator : MonoBehaviour
 			m_Material.SetBuffer("_DNA", m_DNA);
 		}
 
-		var para = new Params[1];
 		for (var it = 0; it < m_IterationsPerUpdate; ++it)
 		{
-			para[0].triCount = m_Triangles;
-			para[0].frame = ++m_Counter;
-			para[0].width = sourceTex.width;
-			para[0].height = sourceTex.height;
-			m_Params.SetData(para);
-
-			m_CS.Dispatch(0, (m_Triangles+63)/64, 1, 1);
-			m_CS.Dispatch(1, 1, 1, 1);
+			m_CS.Dispatch(m_KernelCopy, (m_Triangles+(int)m_GroupsizeCopy-1)/(int)m_GroupsizeCopy, 1, 1);
+			m_CS.Dispatch(m_KernelMutate, 1, 1, 1);
 
 			Graphics.SetRenderTarget(m_RT);
 			GL.Clear(false, true, Color.white);
@@ -153,18 +167,19 @@ public class Trianglomator : MonoBehaviour
 			m_Material.SetPass(0);
 			Graphics.DrawProcedural(MeshTopology.Triangles, m_Triangles * 3);
 
-			m_CS.Dispatch(2, (sourceTex.width+7)/8, (sourceTex.height+7)/8, 1);
+			m_CS.Dispatch(m_KernelScore, (sourceTex.width+(int)m_GroupsizeScoreX-1)/(int)m_GroupsizeScoreX, (sourceTex.height+(int)m_GroupsizeScoreY-1)/(int)m_GroupsizeScoreY, 1);
 		}
 
 		m_Score.GetData(sc);
 		var fitness = (1.0 - sc[0].bestScore / (double) (sourceTex.width * sourceTex.height * 3 * 255)) * 100.0;
 		var time = Time.realtimeSinceStartup;
-		m_Text.text = $"fit {fitness:F2}% time {time:F2}s\niter {sc[0].iterations} impr {sc[0].improvements}\n{m_Log}";
+		m_Text.text = $"fit {fitness:F3}% time {time:F2}s\niter {sc[0].iterations} impr {sc[0].improvements}\n{m_Log}";
 		var ifitness = (int) fitness;
 		if (ifitness > m_GotFitness)
 		{
-			m_Log += $"got {fitness:F2}% at time {time:F2}, iters {sc[0].iterations}\n";
+			m_Log += $"got {fitness:F3}% at time {time:F2}, iters {sc[0].iterations}\n";
 			m_GotFitness = ifitness;
+			Debug.Log(m_Log);
 		}
 
 		Graphics.SetRenderTarget(m_RTBest);
